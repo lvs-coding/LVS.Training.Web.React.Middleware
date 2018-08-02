ba10c848fbb679d0566b70d297aebf9141b9d575
laurent <laurent@langkawi.localdomain>
2018-08-02 10:37:01 +0200

    Sparklines charts added

--- src/reducers/reducer_weather;.js
+++ src/reducers/reducer_weather;.js
@@ -1,4 +1,12 @@
-export default function(state = null, action) {
-    console.log('Action received', action);
+import { FECTH_WEATHER, FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER:
+            // Here we can't mutate the state, it's not allowed, we have to return
+            // a new array with the data of the new city
+            // same as : return state.concat([action.payload.data])
+            return [ action.payload.data, ...state ];
+    }
     return state;
 }
